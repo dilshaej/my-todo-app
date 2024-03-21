b36b017065b29c1ecc0921c474b347f54b9c922a
dilshaej <dilshaejaleel5555@gmail.com>
2024-03-21 12:57:59 +0530

    first commit

--- src/Redux/Slices/todoSlice.js
+++ src/Redux/Slices/todoSlice.js
@@ -1,4 +1,4 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { createSlice, nanoid } from '@reduxjs/toolkit'
 
 const todoSlice = createSlice({
     name: 'todos',
@@ -7,26 +7,26 @@ const todoSlice = createSlice({
       addTodo: {
         reducer(state, action) {
           const { id, text } = action.payload;
-          state.push({ id, text, completed: false });
+          state.push({ id, text, completed: false })
         },
         prepare(text) {
-          return { payload: { id: nanoid(), text } };
+          return { payload: { id: nanoid(), text } }
         }
       },
       toggleTodo(state, action) {
-        const todo = state.find(todo => todo.id === action.payload);
+        const todo = state.find(todo => todo.id === action.payload)
         if (todo) {
-          todo.completed = !todo.completed;
+          todo.completed = !todo.completed
         }
       },
       deleteTodo(state, action) {
-        return state.filter(todo => todo.id !== action.payload);
+        return state.filter(todo => todo.id !== action.payload)
       }
     }
   });
   
-  export const { addTodo, toggleTodo, deleteTodo } = todoSlice.actions;
+  export const { addTodo, toggleTodo, deleteTodo } = todoSlice.actions
   
-  export const selectTodos = state => state.todos;
+  export const selectTodos = state => state.todos
   
-  export default todoSlice.reducer;
+  export default todoSlice.reducer
--- src/Redux/Slices/todoStore.js
+++ src/Redux/Slices/todoStore.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit"
 import todoSlice from '../Slices/todoSlice'
 const todoStore = configureStore({
     reducer: {
